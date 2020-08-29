505a73d0438150778375c67dd17664577073a73f
Michelle Scott <scottmm374@gmail.com>
2020-08-29 16:09:00 -0400

    got dft print to pass

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 """
-Binary search trees are a data structure that enforce an ordering over 
-the data they store. That ordering in turn makes it a lot more efficient 
-at searching for a particular piece of data in the tree. 
+Binary search trees are a data structure that enforce an ordering over
+the data they store. That ordering in turn makes it a lot more efficient
+at searching for a particular piece of data in the tree.
 
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
@@ -9,6 +9,29 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+# from stack import Stack
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    # def __str__(self):
+    #     return f'{self.size} : {self.storage}'
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.append(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.pop()
 
 
 class BSTNode:
@@ -18,6 +41,7 @@ class BSTNode:
         self.right = None
 
     # Insert the given value into the tree
+
     def insert(self, value):
         # compare values to detrmine direction
         if value >= self.value:
@@ -38,94 +62,124 @@ class BSTNode:
                 # Return True if the tree contains the value
                 # False if it does not
 
-    def contains(self, target):
-        # Contains:
-        # Compare target value to node.value
-        if target == self.value:
-            return True
-            # ! CHECKING RIGHT
-        if target > self.value:
-            # If no NODE were DONE
-            if self.right == None:
-                return False
-                # If NODE check target against Right Value
-            elif self.right.value == target:
-                return True
-            else:
-                # Calling function again on RIGHT side of tree
-                self.right.contains(target)
-            # ! CHECKING THE LEFT
-        else:
-            # If no NODE were DONE
-            if self.left == None:
-                return False
-                # If NODE check target against Right Value
-            elif self.left.value == target:
-                return True
-            else:
-                # Calling function again on left side of tree
-                self.left.contains(target)
+    # def contains(self, target):
+    #     # Contains:
+    #     # Compare target value to node.value
+    #     if target == self.value:
+    #         return True
+    #         # ! CHECKING RIGHT
+    #     if target > self.value:
+    #         # If no NODE were DONE
+    #         if self.right == None:
+    #             return False
+    #             # If NODE check target against Right Value
+    #         elif self.right.value == target:
+    #             return True
+    #         else:
+    #             # Calling function again on RIGHT side of tree
+    #             self.right.contains(target)
+    #         # ! CHECKING THE LEFT
+    #     else:
+    #         # If no NODE were DONE
+    #         if self.left == None:
+    #             return False
+    #             # If NODE check target against Right Value
+    #         elif self.left.value == target:
+    #             return True
+    #         else:
+    #             # Calling function again on left side of tree
+    #             self.left.contains(target)
 
 
 #     # Return the maximum value found in the tree
-#     def get_max(self):
-#         pass
 
-#     # Call the function `fn` on the value of each node
-#     def for_each(self, fn):
-#         pass
+    # def get_max(self):
+    #     if self.right is None:
+    #         return self.value
+    #     else:
+    #         max_value = self.right.get_max()
+    #         return max_value
 
-#     # Part 2 -----------------------
+    #         #     # Call the function `fn` on the value of each node
 
-#     # Print all the values in order from low to high
-#     # Hint:  Use a recursive, depth first traversal
-#     def in_order_print(self):
-#         pass
+    # def for_each(self, fn):
 
-#     # Print the value of every node, starting with the given node,
-#     # in an iterative breadth first traversal
-#     def bft_print(self):
-#         pass
+    #     if self.left is not None:
+    #         self.left.for_each(fn)
 
-#     # Print the value of every node, starting with the given node,
-#     # in an iterative depth first traversal
-#     def dft_print(self):
-#         pass
+    #     fn(self.value)
 
-#     # Stretch Goals -------------------------
-#     # Note: Research may be required
-
-#     # Print Pre-order recursive DFT
-#     def pre_order_dft(self):
-#         pass
-
-#     # Print Post-order recursive DFT
-#     def post_order_dft(self):
-#         pass
+    #     if self.right is not None:
+    #         self.right.for_each(fn)
 
 
-# """
-# This code is necessary for testing the `print` methods
-# """
-# bst = BSTNode(1)
-bst = BSTNode(5)
+# Part 2 -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+
+    def in_order_print(self):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self):
+        pass
+
+        # Print the value of every node, starting with the given node,
+        # in an iterative depth first traversal
+
+    def dft_print(self):
+        curr_node = self
+        stack = Stack()
+        stack.push(curr_node)
+
+        while len(stack) > 0:
+            curr_node = stack.pop()
+            print(curr_node.value)
+
+            if curr_node.left is not None:
+                stack.push(curr_node.left)
+
+            if curr_node.right is not None:
+                stack.push(curr_node.right)
+
+    # Stretch Goals -------------------------
+    # Note: Research may be required
+
+    # Print Pre-order recursive DFT
+
+    def pre_order_dft(self):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self):
+        pass
+
+
+"""
+This code is necessary for testing the `print` methods
+"""
+bst = BSTNode(1)
+# bst = BSTNode(8)
+
 
 bst.insert(8)
-# bst.insert(5)
+bst.insert(5)
 bst.insert(7)
 bst.insert(6)
 bst.insert(3)
 bst.insert(4)
 bst.insert(2)
-bst.contains(6)
 
 # bst.bft_print()
-# bst.dft_print()
+print("DFT print")
+bst.dft_print()
 
 # print("elegant methods")
 # print("pre order")
 # bst.pre_order_dft()
 # print("in order")
-# bst.in_order_dft()
+# # bst.in_order_print(bst)
 # print("post order")
 # bst.post_order_dft()
---
+++ binary_search_tree/stack.py
@@ -0,0 +1,158 @@
+"""
+A stack is a data structure whose primary purpose is to store and
+return elements in Last In First Out order.
+
+1. Implement the Stack class using an array as the underlying storage structure.
+   Make sure the Stack tests pass.
+2. Re-implement the Stack class, this time using the linked list implementation
+   as the underlying storage structure.
+   Make sure the Stack tests pass.
+3. What is the difference between using an array vs. a linked list when
+   implementing a Stack?
+"""
+
+# Array Stack
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.insert(0, value)
+
+#     def pop(self):
+#         if len(self.storage) >= 1:
+#             val = self.storage[0]
+#             self.storage.pop(0)
+#             return val
+#         else:
+#             return None
+
+
+# Stack class for Linked List
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    # def __str__(self):
+    #     return f'{self.size} : {self.storage}'
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    # def __str__(self):
+    #     return f'{self.value} : {self.next_node.value}'
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # def __str__(self):
+    #     output = " "
+    #     current = self.head
+    #     while current is not None:
+    #         output += f'{current.value} ->'
+    #         current = current.next_node
+
+    #     return output
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
+    def add_to_head(self, value):
+        if self.head is None:
+            new_node = Node(value)
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node = Node(value)
+            new_node.next_node = self.head
+            self.head = new_node
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        elif self.head.next_node is None:
+            head = self.head
+            self.head = None
+            self.tail = None
+            # print(head.value, "pop value")
+            return head.value
+        else:
+            head = self.head
+            self.head = self.head.next_node
+            # print(head.value, "pop value")
+            return head.value
+
+    def remove_tail(self):
+        if not self.tail:
+            return None
+        current_node = self.head
+        if current_node.next_node is None:
+            tail_value = self.tail.value
+            self.head = None
+            self.tail = None
+            return tail_value
+        else:
+            if current_node.next_node is self.tail:
+                tail_value = current_node.next_node.value
+                self.tail = current_node
+                current_node.next_node = None
+                return tail_value
+
+
+# testing_list = Stack()
+# print(testing_list, "1")
+# testing_list.push(100)
+# print(testing_list.size, "length")
+# print(testing_list, "2")
+# testing_list.push(101)
+# print(testing_list.size, "length")
+# print(testing_list, "3")
+# testing_list.push(105)
+# print(testing_list.size, "length")
+# print(testing_list, "4")
+# testing_list.pop()
+# print(testing_list.size, "length")
+# print(testing_list, "pop 1")
+# testing_list.pop()
+# print(testing_list.size, "length")
+# print(testing_list, "pop 2")
+# testing_list.pop()
+# print(testing_list.size, "length")
+# print(testing_list, "pop 3")
+# print(testing_list, "list")
